c1b356a711a087cba033e03b79e7101c426db8ca
egguterra <allisonrmj@gmail.com>
2022-11-01 16:54:32 -0400

    updated the header file

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $page_title; ?></title>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
