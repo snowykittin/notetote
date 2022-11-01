a83249474a65705cdb5b6004492fa36ed4d9d535
egguterra <allisonrmj@gmail.com>
2022-11-01 17:22:30 -0400

    updated the header file, made a stylesheet, added the fonts, uploaded the logo image, made links in the navbar

--- includes/header.php
+++ includes/header.php
@@ -6,4 +6,16 @@
     <link href="styles/styles.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-
+<nav>
+    <div class="nav-left">
+        <img class="logo-image" src="./images/Logo.png">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Browse All</a>
+        <a href="#">My Collection</a>
+    </div>
+    <div class="nav-right">
+        <a href="#">Shopping Cart</a>
+        <a href="#">Account</a>
+    </div>
+</nav>
