881262416cd13ebb5ac759a8aecb8e887dd7ac5f
egguterra <allisonrmj@gmail.com>
2022-11-15 15:50:11 -0500

    home banner finished html and styling

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 include ('includes/header.php');
 ?>
+
+<div class="content"> <!--content: the white box surrounding the content on the page. should be on every page-->
 <div class="home-banner">
 <h2>Welcome!</h2>
     <p>Welcome to NoteTote. Our site, founded in 2022, is a user-friendly music-based e-commerce site for artists and fans.
     Artists can upload albums of songs, set the album art and price, and see it update live in our browse page. Fans can browse
     our selections and checkout in the shopping cart, and see their purchases appear in their collection.</p>
 </div>
+    <div class="view-button"><p>View Collection</p></div>
+
+
+
+
+
+</div> <!--"content" closing-->
 <?php
 include ('includes/footer.php');
 ?>
