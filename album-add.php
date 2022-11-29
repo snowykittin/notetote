b6344516a6dd549052e78a701c3bb3b81a11c47c
egguterra <allisonrmj@gmail.com>
2022-11-29 15:38:15 -0500

    added album-add: a copy of album-edit without values

--- album-add.php
+++ album-add.php
@@ -1,4 +1,10 @@
 <?php
+/*RIGHT NOW, THIS PAGE IS A COPY OF ALBUM-EDIT
+EXCEPT IT HAS NO VALUES
+Please let me know if there should be any actual changes!
+-Alli
+*/
+
 include ('includes/header.php');
 
 //retrieve album id from a query string
@@ -30,12 +36,12 @@ if (!$query) {
 ?>
 <div class="content">
     <div class="album-details">
-        <input type="text" name="image" value="<?php echo "<img src='", $row['albumIMG'] ,"'>" ?> size="40" required />
+        <input type="text" name="image" size="40" required />
         <div class="album-info">
-            <h2><input type="text" name="title" value="<?php echo $row['album_name'] ?>" required /> - <input type="number" name="price" step="0.01" value="<?php echo $row['price'] ?>" required /> </h2>
-            <p><input type="text" name="artist" value="<?php echo $row['artist'] ?>" required /></p>
-            <p><input type="text" name="description" value="<?php echo $row['description'] ?>" required /></p>
-            <p><input type="number" name="songs" value="<?php echo $row['songs'] ?>" required /></p>
+            <h2><input type="text" name="title"  required /> - <input type="number" name="price" step="0.01" required /> </h2>
+            <p><input type="text" name="artist"  required /></p>
+            <p><input type="text" name="description"  required /></p>
+            <p><input type="number" name="songs"  required /></p>
         </div>
     </div>
     <div class="album-buttons-container">
@@ -53,3 +59,4 @@ $query->close();
 $conn->close();
 include ('includes/footer.php');
 ?>
+
