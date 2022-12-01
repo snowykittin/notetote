cbd13c5d642efe218f69b29ceb956b8ad1b1f3cc
egguterra <allisonrmj@gmail.com>
2022-11-30 19:34:17 -0500

    Styled the edit page

--- album-edit.php
+++ album-edit.php
@@ -29,15 +29,18 @@ if (!$query) {
 
 ?>
 <div class="content">
-    <div class="album-details">
-        <input type="text" name="image" value="<?php echo "<img src='", $row['albumIMG'] ,"'>" ?> size="40" required />
+    <div class="album-details-edit">
         <div class="album-info">
-            <h2><input type="text" name="title" value="<?php echo $row['album_name'] ?>" required /> - <input type="number" name="price" step="0.01" value="<?php echo $row['price'] ?>" required /> </h2>
-            <p><input type="text" name="artist" value="<?php echo $row['artist'] ?>" required /></p>
-            <p><input type="text" name="description" value="<?php echo $row['description'] ?>" required /></p>
-            <p><input type="number" name="songs" value="<?php echo $row['songs'] ?>" required /></p>
+            <input class="input-border input-url" type="text" name="image" value="<?php echo $row['albumIMG']?>" required />
+            <div class="album-info-titles">
+            <input class="input-border input-title" type="text" name="title" value="<?php echo $row['album_name'] ?>" required />
+            <input class="input-border input-title" type="number" name="price" step="0.01" value="<?php echo $row['price'] ?>" required />
+            </div>
+
+            <input class="input-border input-subtitle" type="text" name="artist" value="<?php echo $row['artist'] ?>" required />
+            <textarea class="input-border input-description" type="text" name="description" required><?php echo $row['description'] ?></textarea>
+            <input class="input-border input-subtitle" type="number" name="songs" value="<?php echo $row['songs'] ?>" required />
         </div>
-    </div>
     <div class="album-buttons-container">
         <a href="confirm-album-edit.php"><button class="album-button">Save</button></a>
         <a href="album-details.php?id=<?php echo $row['albumID'] ?>"><button class="album-button">Cancel</button></a>
