8b85746c90d507bb5c69d9eae137e9f00a1f31c9
Summer S <snowykittin@gmail.com>
2022-12-06 15:52:34 -0500

    Added files

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+include ('includes/header.php');
+?>
+
+<!-- HTML HERE -->
+
+<?php
+//// clean up resultsets when we're done with them!
+//$query->close();
+//
+//// close the connection.
+//$conn->close();
+include ('includes/footer.php');
+?>
+
